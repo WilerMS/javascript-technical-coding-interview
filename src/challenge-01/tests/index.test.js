55cdc3b87d79d976bc8d4bb7f5ea30d6a0c4e2b3
WilerMS <wilersoriano@gmail.com>
2023-06-14 02:35:19 +0200

    test for ex 1

---
+++ src/challenge-01/tests/index.test.js
@@ -0,0 +1,17 @@
+const { maxInstructionsWithRedux } = require('..')
+
+/* eslint-disable no-undef */
+describe('challenge-1 getMaxMovementsFromFurtherPoint()', () => {
+  test('[\'R\', \'R\', \'U\', \'U\'] -> 4', () => {
+    expect(maxInstructionsWithRedux(['R', 'R', 'U', 'U'])).toBe(4) // Success
+  })
+  test('[\'R\', \'R\', \'U\', \'L\'] -> 4', () => {
+    expect(maxInstructionsWithRedux(['R', 'R', 'U', 'L'])).toBe(3) // Success
+  })
+  test('[\'L\', \'U\', \'L\', \'L\', \'R\', \'U\'] -> 4', () => {
+    expect(maxInstructionsWithRedux(['L', 'U', 'L', 'L', 'R', 'U'])).toBe(4) // Success
+  })
+  test('[\'R\', \'U\', \'L\', \'D\'] -> 4', () => {
+    expect(maxInstructionsWithRedux(['R', 'U', 'L', 'D'])).toBe(2) // Success
+  })
+})
